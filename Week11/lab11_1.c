c7671c145dfc65a0a8110e18a7397146ef2d4b98
Monthon Sova <igaccmonthon1150@gmail.com>
2026-03-01 15:34:00 +0700

    Update lab11_1.c

--- Week11/lab11_1.c
+++ Week11/lab11_1.c
@@ -4,34 +4,31 @@ int BinSearch(int data[], int n, int find);
 
 int main() {
     int data[6] = { 1, 2, 3, 4, 5, 7 };
-    int n = 6;
-    int find = 5;
-    
+    int n = 6, find = 5;
     int pos = BinSearch(data, n, find);
     
     if (pos != -1) {
-        printf("Found %d at %d\n", find, pos);
-    } else {
-        printf("Not found\n");
+        printf("Found %d at %d", find, pos);
     }
-
+    
     return 0;
 }
 
 int BinSearch(int data[], int n, int find) {
-    int i = 0;
-    int j = n - 1;
-    int m;
-
-    while (i <= j) {
-        m = (i + j) / 2;
-        if (data[m] == find) {
-            return m;
-        } else if (data[m] > find) {
-            j = m - 1;
+    int left = 0;
+    int right = n - 1;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (data[mid] == find) {
+            return mid;
+        } else if (data[mid] < find) {
+            left = mid + 1;
         } else {
-            i = m + 1;
+            right = mid - 1;
         }
     }
+    
     return -1;
 }
